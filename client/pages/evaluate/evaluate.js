fe3a614acc14afe67944611418babe62c0df3435
tangaihua <1525640858@qq.com>
2018-03-29 08:15:39 +0800

    课程评价

---
+++ client/pages/evaluate/evaluate.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    userStars: [
+      '../images/star.png',
+      '../images/grayStar.png',
+      '../images/grayStar.png',
+      '../images/grayStar.png',
+      '../images/grayStar.png'
+    ],
+  },
+  
+
+})
